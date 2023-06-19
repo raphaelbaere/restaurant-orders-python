83fa177f638fd43d1a0d7efa1e8983aae07236a4
raphaelbaere <raphaelbaere@id.uff.br>
2023-06-19 14:30:11 -0300

    Requisito 1 de testes concluído

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,12 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    presunto = Ingredient("Presunto")
+    queijo = Ingredient("Queijo")
+    assert presunto.__eq__(queijo) is False
+    assert presunto.__eq__(presunto) is True
+    assert presunto.__hash__() != queijo.__hash__()
+    assert presunto.__hash__() == presunto.__hash__()
+    assert presunto.name == "Presunto"
+    assert presunto.__repr__() == f"Ingredient('{presunto.name}')"
+    assert presunto.restrictions == set()
