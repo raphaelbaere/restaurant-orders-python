e5fa4a03c044ed2ecf5b3350a8d5408bbca8b4c2
raphaelbaere <raphaelbaere@id.uff.br>
2023-06-19 15:08:10 -0300

    lint fix

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -2,6 +2,7 @@ from src.models.dish import Dish  # noqa: F401, E261, E501
 from src.models.ingredient import Ingredient
 import pytest
 
+
 # Req 2
 def test_dish():
     dish = Dish("hamburguer", 25.00)
@@ -11,26 +12,24 @@ def test_dish():
     assert dish.name == "hamburguer"
     assert dish.price == 25.00
 
-
     with pytest.raises(TypeError):
         Dish("hamburguer", "25")
     with pytest.raises(ValueError):
         Dish("hamburguer", 0)
 
-    
     assert dish.__eq__(dish2) is True
     assert dish.__eq__(dish3) is False
     assert dish.__hash__() == dish2.__hash__()
     assert dish.__hash__() != dish3.__hash__()
     assert dish.__repr__() == f"Dish('{dish.name}', R${dish.price:.2f})"
 
-    ingr1 = Ingredient("queijo mussarela")
-    ingr2 = Ingredient("presunto")
-    dish.add_ingredient_dependency(ingr1, 2)
-    dish.add_ingredient_dependency(ingr2, 2)
-    assert dish.recipe.get(ingr1) == 2
-    assert dish.recipe.get(ingr2) == 2
+    queijo = Ingredient("queijo mussarela")
+    presunto = Ingredient("presunto")
+    dish.add_ingredient_dependency(queijo, 2)
+    dish.add_ingredient_dependency(presunto, 2)
+    assert dish.recipe.get(queijo) == 2
+    assert dish.recipe.get(presunto) == 2
 
-    assert dish.get_restrictions() == ingr1.restrictions.union(
-        ingr2.restrictions
+    assert dish.get_restrictions() == queijo.restrictions.union(
+        presunto.restrictions
     )
